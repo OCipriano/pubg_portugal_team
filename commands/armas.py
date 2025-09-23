3a62024bd59e5c2e077310b64badb253d45165c6
Mikey <code.wish815@passmail.com>
2025-09-23 15:14:19 +0100

    Create armas.py

---
+++ commands/armas.py
@@ -0,0 +1,13 @@
+# ================================================================================ #
+#                                                                                  #
+# Ficheiro:      .env                                                              #
+# Autor:         NunchuckCoder                                                     #
+# Versão:        1.0                                                               #
+# Data:          Julho 2025                                                        #
+# Descrição:     Implementa menus interativos (dropdowns) para consulta de armas   #
+#                do PUBG no Discord. Permite escolher categorias e armas, exibindo #
+#                estatísticas detalhadas através de embeds. Inclui timeout e       #
+#                gestão de interação com os usuários.                              #
+# Licença:       MIT License                                                       #
+#                                                                                  #
+# ================================================================================ #
