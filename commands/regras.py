6549f5874e1a7d9bf7ea8b77a333c70fe4468930
Mikey <code.wish815@passmail.com>
2025-09-23 15:38:34 +0100

    Create regras.py

---
+++ commands/regras.py
@@ -0,0 +1,12 @@
+# ================================================================================ #
+#                                                                                  #
+# Ficheiro:      regras.py                                                         #
+# Autor:         NunchuckCoder                                                     #
+# Versão:        1.0                                                               #
+# Data:          Julho 2025                                                        #
+# Descrição:     Comando /regras para Discord. Mostra as regras do servidor em um  #
+#                embed formatado. Inclui instruções sobre comportamento esperado,  #
+#                respeito, spam, denúncias e conduta dentro do servidor.           #
+# Licença:       MIT License                                                       #
+#                                                                                  #
+# ================================================================================ #
