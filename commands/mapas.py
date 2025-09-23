dc02166f5425e3a89f7b75a5cb3783ce09b88c25
Mikey <code.wish815@passmail.com>
2025-09-23 15:27:22 +0100

    Create mapas.py

---
+++ commands/mapas.py
@@ -0,0 +1,14 @@
+# ================================================================================ #
+#                                                                                  #
+# Ficheiro:      mapas.py                                                          #
+# Autor:         NunchuckCoder                                                     #
+# Versão:        1.0                                                               #
+# Data:          Julho 2025                                                        #
+# Descrição:     Comandos /mapas e /mapaskey para Discord. Permite gerar JSON      #
+#                com mapas semanais do PUBG (EU) a partir de posts do site oficial #
+#                e mostrar os mapas da semana ativa via embed. Inclui parsing de   #
+#                HTML, verificação de permissões, tratamento de datas e interação  #
+#                com o utilizador.                                                 #
+# Licença:       MIT License                                                       #
+#                                                                                  #
+# ================================================================================ #
