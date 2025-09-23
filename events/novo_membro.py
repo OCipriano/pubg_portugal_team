86326ad93ea2573f762e0c426e8485efe4786cfe
Mikey <code.wish815@passmail.com>
2025-09-23 15:11:53 +0100

    Update novo_membro.py

--- events/novo_membro.py
+++ events/novo_membro.py
@@ -1,1 +1,12 @@
-
+# ================================================================================ #
+#                                                                                  #
+# Ficheiro:      .env                                                              #
+# Autor:         NunchuckCoder                                                     #
+# Versão:        1.0                                                               #
+# Data:          Julho 2025                                                        #
+# Descrição:     Evento do bot Discord que envia uma mensagem de boas-vindas       #
+#                por DM a novos membros que entram no servidor. Inclui instruções  #
+#                sobre comandos disponíveis e dicas de utilização do servidor.     #
+# Licença:       MIT License                                                       #
+#                                                                                  #
+# ================================================================================ #
